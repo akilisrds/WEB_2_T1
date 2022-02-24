37cad5b9402b9e618fd9389fd3f1813f74127423
guilherme_borges <guilherme.borges@unidavi.edu.br>
2022-02-24 20:27:53 -0300

    teste

--- index.php
+++ index.php
@@ -7,3 +7,7 @@ $oTag2 = new tag('meta', 'http-equiv="X-UA-Compatible" content="IE=edge"');
 $oTag3 = new tag('meta', 'name="viewport" content="width=device-width, initial-scale=1.0"');
 
 $oHtml = new conteudo($sTag, $sConteudo)
+        
+        
+        
+        sss
